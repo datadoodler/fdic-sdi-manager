ad737c209dfeb46567b3894f2dbffc8b39305114
Kent Merrell <kmerrell@gmail.com>
2016-04-04 10:51:28 -0400

    adding test-data

--- test/1-basic-usage.spec.js
+++ test/1-basic-usage.spec.js
@@ -16,7 +16,7 @@ describe('fdic-sdi-manager module', function () {
             var FdicSdiManager = require('../lib/fdic-sdi-manager.js');
             var fdicSdiManager = new FdicSdiManager('qd');
             expect(fdicSdiManager).to.be.an.instanceof(FdicSdiManager)
-        })
+        });
         it('should take a qdate object in the constructor', function () {
             var qdate = new QDate(20, 4);
             var fdicSdiManager = new FdicSdiManager(qdate);
@@ -27,13 +27,25 @@ describe('fdic-sdi-manager module', function () {
     });
 
     describe('fdic-sdi-manager instance properties', function () {
-        var qdate = new QDate(20, 4);
+        var qdate = new QDate(2015, 1);
         var fdicSdiManager = new FdicSdiManager(qdate);
-        it('should have a qDate property of type QDate')
-        it('should have a read-only stage1Location property assigned in configuartion', function () {
+        it('should have a qDate property of type QDate',function(){
+            expect(fdicSdiManager.qDate).to.be.an.instanceof(QDate)
+        });
+        it('should have a stage1Location property assigned in configuartion', function () {
             var conf=require('config');
             var stage1Location_conf=conf.stage1Location;
+            expect(stage1Location_conf.length).to.be.above(0)
             expect(fdicSdiManager.stage1Location).to.be.equal(stage1Location_conf);
         });
+        it('should have a stage1Filename property derived from qDate and stage1Location', function(){
+            console.log(fdicSdiManager.qDate.string)
+            console.log(fdicSdiManager.stage1Location)
+            console.log(fdicSdiManager.stage1Filename)
+
+        })
+        it('should have a stage1FileExists property (bool)',function(){
+            console.log(fdicSdiManager.stage1FileExists)
+        })
     });
 });
